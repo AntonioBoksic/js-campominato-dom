d1f812513c5ec369090d33a98578c889b47d4b2c
Antonio <antonioboksic1603@gmail.com>
2023-05-10 16:45:57 +0200

    creato array di 16 numeri random che vanno da 1 a 99

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ buttonPlay.addEventListener("click",
         squaresContainer.classList.add("squaresContainer");
         centerContainer.append(squaresContainer);
         
+        // FOR PER CREAZIONE/INSERIMENTO/CLICK QUADRATI
         for (let i = 1; i <= 100; i++) {
             
 
@@ -27,7 +28,7 @@ buttonPlay.addEventListener("click",
             // rendo cliccabile il quadrato
             square.addEventListener("click",
                 function() {
-                    // diventa blu quando lo clicco
+                // diventa blu quando lo clicco
                 square.classList.add("blue")
                 // stampo in console quadrato cliccato
                 console.log(square.innerHTML)
@@ -36,14 +37,50 @@ buttonPlay.addEventListener("click",
             
         };
 
+        //SECONDA PARTE ESERCIZIO
+        //CREAZIONE 16 BOMBE
+        //CREARE UN ARRAY DI 16 NUMERI CASUALI
+        let bombsArray = [];
+        bombsArray = createNumRandomOrderArr(1,16)
+        console.log(bombsArray);
 
     }
 );
 
 
 // FUNZIONI 
+
+// crea elemento "tag" e aggiungigli una "classe"
 function createSquare(tagType , classToAdd){
     const newElement = document.createElement(tagType);
     newElement.classList.add(classToAdd);
     return newElement;
 }
+
+// crea un array di numeri random che vanno da min a max
+function createNumRandomOrderArr (min, max) {
+
+    // creiamo un array interno alla funzione
+    const intArr = [];
+
+    // ora dobbiamo popolarlo 
+    // finchè la lungheza dell'array è minore al "max" selezionato
+    while (intArr.length < max) {
+        // crea numero random tra min e numero quadrati
+        const nuovoNum = RandomNumMinMax(min, 99);
+        // se il numero è presente nell'array non fare nulla
+        if(intArr.includes(nuovoNum)){
+        // se il numero NON è presente nell'array pushalo dentro 
+        } else {
+        intArr.push(nuovoNum);
+        }
+    }
+    return intArr;
+}
+
+// crea numero random che vada da numMin a numMax
+function RandomNumMinMax (numMin, numMax) {
+    return Math.floor(Math.random() * (numMax - numMin + 1) + numMin)
+ }
+
+ //console.log(RandomNumMinMax(1,3));
